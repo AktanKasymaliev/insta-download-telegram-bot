5fdf2dc910db290f7ed2bd5565981bf91773fde8
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-08-13 13:00:58 +0600

    refactor

--- parse_insta.py
+++ parse_insta.py
@@ -1,9 +1,6 @@
 from bs4 import BeautifulSoup as bs
-from time import sleep, time
+from time import sleep
 import os, fake_useragent, datetime, requests
-from threading import Thread
-
-
 
 def request(url, head=None):
     r = requests.get(url, headers=head)
@@ -13,14 +10,13 @@ def request(url, head=None):
 def delete_file(filename):
     os.remove(filename)
 
-def write_file(file_name):
-    file_format = file_name.split('/')[-1].split('?')[0].split('.')[-1]
+def write_file(url):
+    file_format = url.split('/')[-1].split('?')[0].split('.')[-1]
     created_name = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     full_name = f"{created_name}.{file_format}"
     with open(full_name, 'wb') as file:
-        file.write(request(file_name).content)
-    return full_name  
-
+        file.write(request(url).content)
+    return url
 
 def main(link):
     fake_headers = fake_useragent.UserAgent().random
@@ -31,8 +27,8 @@ def main(link):
         metaTag = soup.find_all('meta', {'property':'og:video'})
     else:
         metaTag = soup.find_all('meta', {'property':'og:image'})
-    file_name = write_file(metaTag[0]["content"])
-    return file_name
+    file_ = write_file(metaTag[0]["content"])
+    return file_
 
 if __name__ == '__main__':
     main()
